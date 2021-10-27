541106355e24f05469e651a90efd870f73d9ed1d
JMG001 <91408617+JMG001@users.noreply.github.com>
2021-10-28 10:37:09 +1100

    Update SendSMS.js

--- SendSMS.js
+++ SendSMS.js
@@ -8,7 +8,7 @@ client.messages
   .create({
      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
      from: '+16515041883',
-     statusCallback: 'https://4c20-202-169-118-14.ngrok.io/MessageStatus',
+     statusCallback: 'https://xxx.ngrok.io/MessageStatus',
      to: process.env.TWILIO_MY_NUMBER
    })
   .then(message => console.log(' Message SID: ',message.sid,'\n','Sent Date: ',message.date_sent,'\n','To: ',message.to,'\n','Body: ',message.body,'\n','Status: ',message.status));
